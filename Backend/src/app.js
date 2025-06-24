315a335a6203acd40394f6c215d1b3a598283799
Nitin-M-1 <condyman1234@gmail.com>
2025-06-24 12:58:58 +0530

    exploring Get,  post, delete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,35 +1,19 @@
 const express = require("express");
-// creating server
+
 const app = express();
+
 const port = 3000;
-
-// middleware
-// app.use((req, res) => {
-//   res.send("Hello From the server!..");
-// });
-
-app.get("/", (req, res) => {
-  res.json([
-    {
-      message: "message send ",
-    },
-  ]);
-});
-app.get("/random.text", (req, res) => {
-  res.send("random.text");
+app.get("/user", (req, res) => {
+  res.send("getting information about user ");
 });
 
-app.all("/app", (req, res) => {
-  console.log("you coming from", req.method);
-  res.send("Hello123").status(200);
+app.get("/api", (req, res) => {
+  res.status(404).send("404 Not Found");
+});
+app.delete("/user", (req, res) => {
+  res.status(200).send("delete-user-data");
 });
 
-const add = (req, res, next) => {
-  console.log("the response will be sent by the next function ...");
-  next();
-};
-app.get("/example/b", add, (req, res) => {
-  res.send("Hello from B!");
+app.listen(port, () => {
+  console.log("Server is running..");
 });
-
-app.listen(port);
